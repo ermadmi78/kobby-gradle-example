95b7c2cdab4d998c1f48be7f04779a93b855f878
dermakov <ermadmi78@gmail.com>
2021-11-18 20:12:27 +0300

    Kobby release 1.3.1

--- cinema-api/build.gradle.kts
+++ cinema-api/build.gradle.kts
@@ -5,7 +5,7 @@ description = "Cinema API Example"
 plugins {
     kotlin("jvm")
     `java-library`
-    id("io.github.ermadmi78.kobby") version "1.3.0"
+    id("io.github.ermadmi78.kobby") version "1.3.1"
 }
 
 kobby {
