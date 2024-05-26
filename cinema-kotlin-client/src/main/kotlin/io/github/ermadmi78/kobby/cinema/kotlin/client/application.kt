fa51398f08080acca09d3eab1b60883a4db55d4a
dermakov <ermadmi78@gmail.com>
2024-05-26 15:26:37 +0300

    Kobby release 4.0.1

--- cinema-kotlin-client/src/main/kotlin/io/github/ermadmi78/kobby/cinema/kotlin/client/application.kt
+++ cinema-kotlin-client/src/main/kotlin/io/github/ermadmi78/kobby/cinema/kotlin/client/application.kt
@@ -402,8 +402,7 @@ class Application : CommandLineRunner {
                     override fun <T : Any> deserialize(content: String, contentType: KClass<T>): T =
                         mapper.readValue(content, contentType.java)
                 },
-                { mapOf("Authorization" to "Basic YWRtaW46YWRtaW4=") },
-                subscriptionReceiveTimeoutMillis = 23000
+                { mapOf("Authorization" to "Basic YWRtaW46YWRtaW4=") }
             ) {
                 println(">> ${it.query}")
                 println(">> ${it.variables}")
