30f9f17225ae76164d09503f76300eff67bae516
dermakov <ermadmi78@gmail.com>
2026-01-14 15:41:58 +0300

    Upgrade to Spring Boot 3.5.9

--- cinema-api/build.gradle.kts
+++ cinema-api/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
     kotlin("jvm")
     kotlin("plugin.serialization")
     `java-library`
-    id("io.github.ermadmi78.kobby") version "5.2.3"
+    id("io.github.ermadmi78.kobby") version "5.3.1"
 }
 
 kobby {
